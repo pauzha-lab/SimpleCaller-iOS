ac44bf4fdbf0ce22e15ea8548dcd3790da0e5a45
pauzha <support@pauzha.com>
2021-06-22 22:27:07 -0700

    Initial Commit

---
+++ SimeplCaller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimeplCaller
+//
+//  Created by pzdev on 22/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
